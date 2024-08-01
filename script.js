1000115a3982c9905dc3207a7a46b3e4958e1bef
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2024-08-01 13:41:25 +1200

    Expected Testing
    Tried to test expected units and again the prompt did not work.

--- script.js
+++ script.js
@@ -6,18 +6,22 @@ let attemps = [
 
 function rockPaperScissors () {
     let userAttempt = prompt ("Let's play a game; Paper, Scissors, Rock. Answer below with either 'paper', 'scissors' or 'rock'!")
-            
-            
+
     randomGeneratorRange = 3
     let index = Math.floor (Math.random() *randomGeneratorRange)
 
+    if (index = userAttempt) {
+        alert('We tied! Try again')
+    }
+            
 
-//What I need to do: Make an if for if they say rock paper scissors, all caps, lowercase use && to simplify this. And if the if equals scissors check if it wins or loses against my attempt. Eg. All scissors lose against rock and win against paper. First part is to make sure all slightly wrong answers work, and then check if they said scissors and i said rock make them lose but if they said scissors and i said rock make them win etc.
+
+//What I need to do: Make an if for if they say rock paper scissors, all caps, lowercase use && to simplify this. And if the if equals scissors check if it wins or loses against my attempt. Eg. All scissors lose against rock and win against paper. First part is to make sure all slightly wrong answers work, and then check if they said scissors and i said rock make them lose but if they said scissors and i said rock make them win etc. See if this is actually the best way to do this otherwise SIMPLIFY it.
 
 
 
 
-    while (userAttempt != attempts ) {
+    while (userAttempt != attempts[index] ) {
         
     }
 
